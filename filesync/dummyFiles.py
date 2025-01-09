84e921fe5a2ff0d14bfaf1446a0f0381bb6fa98d
Jerome Chiu <jerome_chiu@wistron.com>
2025-01-09 18:15:20 +0800

    rewrite filesync

--- filesync/dummyFiles.py
+++ filesync/dummyFiles.py
@@ -2,20 +2,20 @@ import time
 import os
 from random import randint
 
-count = 10
-folders = ['jerome', 'jerome1', 'jerome2']
+count = 500
+folders = ['user1']
 
-root = '/Users/kuan-linchiu/Documents/Codes/sftp-HA/sftp/data'
+root = '/mnt/c/Users/11107390/Downloads/dummydata'
 for i in range(count):
     print(i)
-    sub = str(randint(1,2))
+    sub = str(randint(1, 2))
     for folder in folders:
         if not os.path.exists(os.path.join(root, folder, sub)):
             os.makedirs(os.path.join(root, folder, sub))
-        
-        
+
         size = randint(500, 10000)
-        f = open(os.path.join(root, folder, sub, f'{int(time.time())}.txt'), "w")
+        f = open(os.path.join(root, folder, sub,
+                 f'{int(time.time())}.txt'), "w")
         for _ in range(size):
             f.write(f'{int(time.time())}\n')
         f.close()
--- filesync/filesync.py
+++ filesync/filesync.py
@@ -2,13 +2,13 @@ import os
 import pysftp
 import yaml
 import subprocess
+from multiprocessing import Event
 import multiprocessing as mp
-import hashlib
-import json
 from tempfile import TemporaryDirectory
 from time import strftime, localtime
 import time
 from stat import S_ISDIR, S_ISREG
+import time
 
 configName = 'config.yaml'
 
@@ -41,12 +41,11 @@ class sftp:
         finally:
             print(f"Connected to {self.hostname} as {self.username}.")
 
-
     def disconnect(self):
         """Closes the sftp connection"""
         self.connection.close()
         print(f"Disconnected from host {self.hostname}")
-    
+
     def listdir(self, remote_path):
         """lists all the files and directories in the specified path and returns them"""
         for obj in self.connection.listdir(remote_path):
@@ -56,7 +55,7 @@ class sftp:
         """lists all the files and directories (with their attributes) in the specified path and returns them"""
         for attr in self.connection.listdir_attr(remote_path):
             yield attr
-            
+
     def listfiles(self, remote_path):
         """lists all files in sftp with directory and returns them"""
         files = list()
@@ -66,26 +65,28 @@ class sftp:
                 # print(file.filename, file.st_mode, file.st_size, file.st_atime, file.st_mtime)
                 if S_ISDIR(file.st_mode):
                     # print(file.filename + " is folder")
-                    files.extend(self.listfiles(os.path.join(remote_path,file.filename)))
+                    files.extend(self.listfiles(
+                        os.path.join(remote_path, file.filename)))
                 elif S_ISREG(file.st_mode):
-                    
+
                     if list(file.filename)[0] != '.':
                         # print(file.filename + " is file")
-                        files.append([remote_path, file.filename, file.st_mtime])
+                        files.append(
+                            [remote_path, file.filename, file.st_mtime])
 
         except Exception as ex:
             print(f'Get File List error: {ex}')
         return files
-    
+
     def fileAttr(self, remote_path, filename):
         attr = None
         full_filename = os.path.join(remote_path, filename)
         try:
             attr = self.connection.stat(full_filename)
         except FileNotFoundError:
-            print('No Such file at target') 
+            print('No Such file at target')
         return attr
-    
+
     def upload(self, source_local_path, remote_dir, remote_filename):
         """
         Uploads the source files from local to the sftp server.
@@ -105,21 +106,13 @@ class sftp:
                     self.connection.chdir(f)
             try:
                 # Upload file from SFTP
-                self.connection.put(localpath=source_local_path, remotepath=os.path.join(remote_dir,remote_filename), confirm=True)
+                self.connection.put(localpath=source_local_path, remotepath=os.path.join(
+                    remote_dir, remote_filename), confirm=True)
                 print("upload completed")
 
             except Exception as err:
                 raise Exception(err)
-        
 
-
-                
-                
-            
-        
-        
-
-        
     def download(self, remote_path, target_local_path):
         """
         Downloads the file from remote sftp server to local.
@@ -145,82 +138,158 @@ class sftp:
         except Exception as err:
             raise Exception(err)
 
+
 def doSync(mode, master, standby, masterFiles, standbyFiles):
     with TemporaryDirectory(prefix="sftp_") as tmpdirname:
         if mode == 'single':
-            standbyFilesNoMktime = [[x1,x2] for [x1,x2,_] in standbyFiles]
+            standbyFilesNoMktime = [[x1, x2] for [x1, x2, _] in standbyFiles]
             """Master files first"""
             for directory, filename, mktime in masterFiles:
-                print(f'Check file {os.path.join(directory,filename)} of Standby')
-                print(f'{[directory, filename, mktime]}')
+                print(
+                    f'Check file {os.path.join(directory,filename)} of Standby')
+                # print([directory, filename])
+                # print(f'standbyFilesNoMktime {standbyFilesNoMktime}')
                 if [directory, filename] not in standbyFilesNoMktime:
                     print(f'File {filename} in Master ----> standby')
-                    master.download(os.path.join(directory, filename), os.path.join(tmpdirname, filename))
-                    standby.upload(os.path.join(tmpdirname, filename), directory, filename)
+                    master.download(os.path.join(directory, filename),
+                                    os.path.join(tmpdirname, filename))
+                    standby.upload(os.path.join(
+                        tmpdirname, filename), directory, filename)
         else:
             """Not yet implement"""
             pass
 
-def monitorConfig(q):
+
+def monitorConfig(event):
     lastModoft = os.path.getmtime(configName)
     while True:
         if lastModoft != os.path.getmtime(configName):
-            lastModoft = os.path.getmtime(configName)   
-            q.put('changed')
-            print(f'config.yaml is changed at {strftime('%Y-%m-%d %H:%M:%S', \
-                localtime(os.path.getmtime(configName)))}')
+            lastModoft = os.path.getmtime(configName)
+            event.set()
+            print(
+                f'config.yaml is changed at {strftime("%Y-%m-%d %H:%M:%S", localtime(os.path.getmtime(configName)))}')
         time.sleep(1)
 
+
 def syncFile(config):
-    print('Start to run Sync function')
-    with open(config,'r') as fp:
+    print('Start to run Sync admin files')
+    with open(config, 'r') as fp:
         configContext = yaml.load(fp, Loader=yaml.FullLoader)
         masterIP = configContext['Master']['IP']
         masterPORT = configContext['Master']['PORT']
         masterUSER = configContext['Master']['USERNAME']
         masterPASSWD = configContext['Master']['PASSWORD']
-        syncMETHOD = configContext['Master']['SYNCMETHOD'] # Currently, the program only support single direction synchronization.
-        
+        # Currently, the program only support single direction synchronization.
+        syncMETHOD = configContext['Master']['SYNCMETHOD']
+
         standyIP = configContext['Standby']['IP']
         standyPORT = configContext['Standby']['PORT']
         standyUSER = configContext['Standby']['USERNAME']
         standyPASSWD = configContext['Standby']['PASSWORD']
-    
-    master = sftp(hostname = masterIP,port = masterPORT, username = masterUSER,password = masterPASSWD)
-    master.connect()
-    standby = sftp(hostname = standyIP,port = standyPORT, username = standyUSER,password = standyPASSWD)
-    standby.connect()
-    
-    while True:
-            
 
-        root = '/'
+    while True:
+        master = sftp(hostname=masterIP, port=masterPORT,
+                      username=masterUSER, password=masterPASSWD)
+        try:
+            master.connect()
+        except Exception as ex:
+            print(f'Connect to Master error: {ex}')
+            return
+        standby = sftp(hostname=standyIP, port=standyPORT,
+                       username=standyUSER, password=standyPASSWD)
+        try:
+            standby.connect()
+        except Exception as ex:
+            print(f'Connect to Standby error: {ex}')
+            return
+
+        root = f'/'
         masterFiles = master.listfiles(root)
         standbyFiles = standby.listfiles(root)
         print('______________________')
         print(f'masterFiles: {masterFiles}')
         print(f'standbyFiles: {standbyFiles}')
-        doSync(syncMETHOD, master=master, standby=standby, masterFiles=masterFiles, standbyFiles=standbyFiles)
+
+        # Sync user account info
+        doSync(syncMETHOD, master=master, standby=standby,
+               masterFiles=masterFiles, standbyFiles=standbyFiles)
+
+        standby.disconnect()
+        # Sync user's files
+        with TemporaryDirectory(prefix="sftp_") as tmpdirname:
+            master.download(os.path.join(
+                '/admin', 'users.yaml'), f'{tmpdirname}/users.yaml')
+            master.disconnect()
+
+            with open(f'{tmpdirname}/users.yaml', 'r') as fp:
+                users = yaml.load(fp, Loader=yaml.FullLoader)
+                # print(users)
+                # """{'users':
+                # {'ADMIN': {'name': 'admin', 'password': 'AhqTi3W4baG8U0u', 'admin': True},
+                # 1: {'name': 'user1', 'password': '1234'}, 2: {'name': 'user2', 'password': '1234'}
+                # }}}"""
+                for user in users['users']:
+                    if users['users'][user]['name'] != 'admin':
+                        username = users['users'][user]['name']
+                        password = users['users'][user]['password']
+                        print(f'User {username} is going to sync')
+                        master = sftp(hostname=masterIP, port=masterPORT,
+                                      username=username, password=password)
+                        try:
+                            master.connect()
+                        except Exception as ex:
+                            print(f'Connect to Master error: {ex}')
+                            continue
+                        standby = sftp(hostname=standyIP, port=standyPORT,
+                                       username=username, password=password)
+                        try:
+                            standby.connect()
+                        except Exception as ex:
+                            print(f'Connect to Standby error: {ex}')
+                            continue
+                        root = f'/{username}'
+                        masterFiles = master.listfiles(root)
+                        standbyFiles = standby.listfiles(root)
+                        print('______________________')
+                        print(f'masterFiles: {masterFiles}')
+                        print(f'standbyFiles: {standbyFiles}')
+
+                        # Sync user account info
+                        doSync(syncMETHOD, master=master, standby=standby,
+                               masterFiles=masterFiles, standbyFiles=standbyFiles)
+                        master.disconnect()
+                        standby.disconnect()
+
         time.sleep(10)
-        
-        
+
 
 def runAll():
-    q = mp.Queue()
-    monitor = mp.Process(target=monitorConfig, args=(q,))
+    # q = mp.Queue()
+    config_flag = Event()
+    working_flag = Event
+    # monitor = mp.Process(target=monitorConfig, args=(q,))
+    monitor = mp.Process(target=monitorConfig, args=(config_flag,))
     sync = mp.Process(target=syncFile, args=(configName,))
-    
+
     monitor.start()
     sync.start()
-    
+
     while True:
-        if q.get() == 'changed':
+        if config_flag.is_set() and not working_flag.is_set():
             sync.terminate()
             sync = mp.Process(target=syncFile, args=(configName,))
             sync.start()
-            
+            config_flag.clear()
+        elif not sync.is_alive():
+            print('Sync Process is not alive')
+            sync = mp.Process(target=syncFile, args=(configName,))
+            sync.start()
+        elif not monitor.is_alive():
+            print('Monitor Process is not alive')
+            monitor = mp.Process(target=monitorConfig, args=(config_flag,))
+            monitor.start()
         else:
-            time.sleep(0.5)
+            time.sleep(5)
 
 
 if __name__ == '__main__':
@@ -229,4 +298,3 @@ if __name__ == '__main__':
     run = mp.Process(target=runAll, args=())
     run.start()
     run.join()
-    
